18ea26c9f362cbe594941b66c75e0bacc1b874e5
eddienst <edoardo.nosotti@gmail.com>
2025-03-22 17:18:42 +0100

    Add logging wrapper and tests

---
+++ src/lib/logging.js
@@ -0,0 +1,73 @@
+const logToCloud = (...args) => {
+  try {
+    /**
+     * `Logger` is a global object provided by Google Apps Script.
+     * It is used to log messages to the Stackdriver Logging service.
+     * It is only available in the Google Apps Script environment.
+     */
+    Logger.log(...args);
+  } catch (error) {
+    // Fallback to `console.log` if `Logger` is not available.
+    console.log(...args);
+  }
+}
+
+/**
+ * This wrapper class allows to transparently switch between
+ * logging to the console and logging to the Stackdriver Logging service
+ * in Apps Script. It also allows local testing of the application
+ * outside of Apps Script, trapping any errors that may occur because
+ * of missing objects that only exist in the Apps Script environment.
+ */
+class Logging {
+  /**
+   * Constructor
+   * @param {Config} config Config reader object
+   */
+  constructor(config) {
+    this.useCloudLogging = config.getUserProp("USE_CLOUD_LOGGING", false);
+  }
+
+  log(...args) {
+    if (this.useCloudLogging) {
+      logToCloud(...args);
+    } else {
+      console.log(...args);
+    }
+  }
+
+  info(...args) {
+    if (this.useCloudLogging) {
+      logToCloud(...args);
+    } else {
+      console.info(...args);
+    }
+  }
+
+  warn(...args) {
+    if (this.useCloudLogging) {
+      logToCloud(...args);
+    } else {
+      console.warn(...args);
+    }
+  }
+
+  error(...args) {
+    if (this.useCloudLogging) {
+      logToCloud(...args);
+    } else {
+      console.error(...args);
+    }
+  }
+
+  debug(...args) {
+    if (this.useCloudLogging) {
+      logToCloud(...args);
+    } else {
+      // `console.debug` is not available in the Google Apps Script environment.
+      console.log(...args);
+    }
+  }
+}
+
+module.exports = Logging;
---
+++ test/lib/logging.test.js
@@ -0,0 +1,45 @@
+const log = jest.spyOn(console, "log").mockImplementation(() => {});
+const info = jest.spyOn(console, "info").mockImplementation(() => {});
+const warn = jest.spyOn(console, "warn").mockImplementation(() => {});
+const error = jest.spyOn(console, "error").mockImplementation(() => {});
+
+const Logging = require("../../src/lib/logging");
+
+describe("lib/logging", () => {
+  beforeEach(() => {
+    log.mockClear();
+    info.mockClear();
+    warn.mockClear();
+    error.mockClear();
+  });
+
+  it("should log to console", () => {
+    const logging = new Logging({ getUserProp: () => false });
+    logging.log("log");
+    logging.info("info");
+    logging.warn("warn");
+    logging.error("error");
+    logging.debug("debug");
+
+    expect(log).toBeCalledWith("log");
+    expect(info).toBeCalledWith("info");
+    expect(warn).toBeCalledWith("warn");
+    expect(error).toBeCalledWith("error");
+    expect(log).toBeCalledWith("debug");
+  });
+
+  it("should log to cloud", () => {
+    const logging = new Logging({ getUserProp: () => true });
+    logging.log("log");
+    logging.info("info");
+    logging.warn("warn");
+    logging.error("error");
+    logging.debug("debug");
+
+    expect(log).toBeCalledWith("log");
+    expect(log).toBeCalledWith("info");
+    expect(log).toBeCalledWith("warn");
+    expect(log).toBeCalledWith("error");
+    expect(log).toBeCalledWith("debug");
+  });
+});

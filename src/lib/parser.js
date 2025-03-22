19f5243311a80f2f742c43c11a61390300715605
eddienst <edoardo.nosotti@gmail.com>
2025-03-22 16:16:53 +0100

    Add Parser library and tests

---
+++ src/lib/parser.js
@@ -0,0 +1,79 @@
+/**
+ * This class contains helper functions to parse
+ * the body of e-mail messages and extract relevant
+ * information such as the total amount of a purchase
+ * receipt and the purchase date.
+ */
+class Parser {
+  /**
+   * Returns a regular expression to find any currency
+   * values in the body of a message.
+   * @return {RegExp}
+   */
+  getRegexMatchCurrency() {
+    return new RegExp(/(\d{1,}(,|\.)\d{1,2}( *?)(€|\$))|(€|\$)( *?)(\d{1,}(,|\.)\d{1,2})/, "g");
+  }
+
+  /**
+   * Returns a regular expression to parse currency
+   * values to a valid float value.
+   * @return {RegExp}
+   */
+  getRegexParseAmount() {
+    return new RegExp(/(\d+(\.|,)\d+)/, "g");
+  }
+
+  /**
+   * Simple helper function to parse the total amount
+   * from a purchase receipt. It assumes that the
+   * last currency value in the receipt body is the
+   * total amount, net of any discounts applied or
+   * wallet credit deducted from the amount due.
+   * Example of a receipt body:
+   * ```
+   * Item #1:  10,00€
+   * Item #2:  15,00€
+   * Discount:  5,00€
+   * Total:  20,00€
+   * ```
+   * In this case, the total amount is 20,00€.
+   * @param {GMailMessage} message Gmail message object
+   * @param {Function} bodyHandler Function to retrieve the body of the message
+   * @return {string | undefined}
+   */
+  findTotalAmount(message, bodyHandler) {
+    const body = message[bodyHandler]();
+    const items = body.match(this.getRegexMatchCurrency());
+
+    if (items && items.length > 0) {
+      return items.reverse()[0];
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Simple helper function to parse the purchase date.
+   * It assumes the date of the receipt sent via e-mail
+   * is the purchase date.
+   * @param {GMailMessage} message Gmail message object
+   * @return {Date}
+   */
+  findPurchaseDate(message) {
+    return message.getDate();
+  }
+
+  /**
+   * Sanitizes currency values retrieved from
+   * the body of the messages to valid float values.
+   * @param {string} value Currency value
+   * @return {string}
+   */
+  sanitizeFloatString(value) {
+    return `${value}`
+      .match(this.getRegexParseAmount())[0]
+      .replace(",", ".");
+  }
+}
+
+module.exports = Parser;
---
+++ test/lib/parser.test.js
@@ -0,0 +1,79 @@
+const Parser = require("../../src/lib/parser");
+
+const TEST_STRINGS = [
+  "10,00€",
+  "10.00€",
+  "10,00$",
+  "10.00$",
+  "€10,00",
+  "€10.00",
+  "$10,00",
+  "$10.00",
+  "10,00  €",
+  "10.00  €",
+  "10,00  $",
+  "10.00  $",
+  "€  10,00",
+  "€  10.00",
+  "$  10,00",
+  "$  10.00",
+  "Total amount due: 10,00€",
+  "10,00€ received, thanks!",
+  "Total amount of 10,00€ received",
+  "10,00€ is the total amount"
+];
+
+describe("lib/parser", () => {
+  let parser;
+
+  beforeEach(() => {
+    parser = new Parser();
+  });
+
+  test("getRegexMatchCurrency should return a regex to match currency values", () => {
+    const regex = parser.getRegexMatchCurrency();
+    expect(regex).toBeInstanceOf(RegExp);
+    TEST_STRINGS.forEach((testString) => {
+      expect(testString.match(regex)[0].replace(",", ".").includes("10.00")).toBe(true);
+    });
+  });
+
+  test("getRegexParseAmount should return a regex to parse currency values", () => {
+    const regex = parser.getRegexParseAmount();
+    expect(regex).toBeInstanceOf(RegExp);
+    expect("10,00".match(regex)).toEqual(["10,00"]);
+    TEST_STRINGS.forEach((testString) => {
+      expect(testString.match(regex)[0].replace(",", ".")).toBe("10.00");
+    });
+  });
+
+  test("findTotalAmount should return the last currency value in the message body", () => {
+    const message = {
+      getBody: () => "Item #1:  10,00€\nItem #2:  15,00€\nDiscount:  5,00€\nTotal:  20,00€"
+    };
+    const totalAmount = parser.findTotalAmount(message, "getBody");
+    expect(totalAmount).toBe("20,00€");
+  });
+
+  test("findTotalAmount should return undefined if no currency value is found", () => {
+    const message = {
+      getBody: () => "No currency value here"
+    };
+    const totalAmount = parser.findTotalAmount(message, "getBody");
+    expect(totalAmount).toBeUndefined();
+  });
+
+  test("findPurchaseDate should return the date of the message", () => {
+    const date = new Date();
+    const message = {
+      getDate: () => date
+    };
+    const purchaseDate = parser.findPurchaseDate(message);
+    expect(purchaseDate).toBe(date);
+  });
+
+  test("sanitizeFloatString should convert currency string to valid float string", () => {
+    const sanitizedValue = parser.sanitizeFloatString("10,00€");
+    expect(sanitizedValue).toBe("10.00");
+  });
+});

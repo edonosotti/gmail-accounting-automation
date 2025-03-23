30b6a8f8f05bb622334da03e7004f6cb0cab740d
eddienst <edoardo.nosotti@gmail.com>
2025-03-23 15:30:41 +0100

    Update Config library.

--- src/lib/config.js
+++ src/lib/config.js
@@ -40,6 +40,63 @@ class Config {
       return ((process ?? {}).env ?? {})[`GAS_DOC_${name}`] ?? defaultValue;
     }
   }
+
+  /**
+   * Returns the ID of the user's Google Cloud Platform project
+   * bound to the script.
+   * @return {string}
+   */
+  getGcpProjectId() {
+    return this.getUserProp('GCP_PROJECT_ID');
+  }
+
+  /**
+   * Returns the ID of the BigQuery dataset to be optionally
+   * used to store the data.
+   * @return {string}
+   */
+  getBqDatasetId() {
+    return this.getUserProp('BQ_DATASET_ID', 'gmail_accounting');
+  }
+
+  /**
+   * Returns the location of the BigQuery dataset to be optionally
+   * used to store the data.
+   * @return {string}
+   */
+  getBqDatasetLocation() {
+    return this.getUserProp('BQ_DATASET_LOCATION');
+  }
+
+  /**
+   * Returns the ID of the BigQuery table to be optionally
+   * used to store the data.
+   * @return {string}
+   */
+  getBqTableId() {
+    return this.getUserProp('BQ_TABLE_ID', 'expenses');
+  }
+
+  /**
+   * Returns the label that will be appended to each message
+   * that has been processed to filter them out from the
+   * next processing.
+   * @return {string}
+   */
+  getGmailLabel() {
+    return this.getUserProp('GMAIL_LABEL', 'Expenses');
+  }
+
+  /**
+   * Returns the maximum number of messages that will be
+   * processed in one batch, per each merchant.
+   * Returns `1` if the value is not a positive integer.
+   * @return {number}
+   */
+  getProcessingBatchSize() {
+    const value = parseInt(this.getUserProp('PROCESSING_BATCH_SIZE', 1000));
+    return !isNaN(value) && value > 0 ? value : 1;
+  }
 }
 
 module.exports = Config;
--- test/lib/config.test.js
+++ test/lib/config.test.js
@@ -11,6 +11,19 @@ process.env[`GAS_SCRIPT_${TEST_SCRIPT_PROP_NAME}`] = TEST_SCRIPT_PROP_VALUE;
 process.env[`GAS_USER_${TEST_USER_PROP_NAME}`] = TEST_USER_PROP_VALUE;
 process.env[`GAS_DOC_${TEST_DOC_PROP_NAME}`] = TEST_DOC_PROP_VALUE;
 
+const TEST_USER_GCP_PROJECT_ID = `VAL_${Math.random() * 1000}`;
+const TEST_USER_BQ_DATASET_ID = `VAL_${Math.random() * 1000}`;
+const TEST_USER_BQ_DATASET_LOCATION = `VAL_${Math.random() * 1000}`;
+const TEST_USER_BQ_TABLE_ID = `VAL_${Math.random() * 1000}`;
+const TEST_USER_GMAIL_LABEL = `VAL_${Math.random() * 1000}`;
+const TEST_USER_PROCESSING_BATCH_SIZE = `${Math.floor((Math.random() * 1000) + 1)}`;
+process.env[`GAS_USER_GCP_PROJECT_ID`] = TEST_USER_GCP_PROJECT_ID;
+process.env[`GAS_USER_BQ_DATASET_ID`] = TEST_USER_BQ_DATASET_ID;
+process.env[`GAS_USER_BQ_DATASET_LOCATION`] = TEST_USER_BQ_DATASET_LOCATION;
+process.env[`GAS_USER_BQ_TABLE_ID`] = TEST_USER_BQ_TABLE_ID;
+process.env[`GAS_USER_GMAIL_LABEL`] = TEST_USER_GMAIL_LABEL;
+process.env[`GAS_USER_PROCESSING_BATCH_SIZE`] = TEST_USER_PROCESSING_BATCH_SIZE;
+
 const Config = require('../../src/lib/config');
 describe('lib/config', () => {
   let config;
@@ -52,6 +65,48 @@ describe('lib/config', () => {
     it('should return the default value if the document property does not exist', () => {
       const value = config.getDocProp(`_NONEXISTENT_${TEST_DOC_PROP_NAME}`, 'defaultValue');
       expect(value).toBe('defaultValue');
+    });
+  });
+
+  describe('getGcpProjectId', () => {
+    it('should return the user property GCP_PROJECT_ID', () => {
+      const value = config.getGcpProjectId();
+      expect(value).toBe(TEST_USER_GCP_PROJECT_ID);
+    });
+  });
+
+  describe('getBqDatasetId', () => {
+    it('should return the user property BQ_DATASET_ID', () => {
+      const value = config.getBqDatasetId();
+      expect(value).toBe(TEST_USER_BQ_DATASET_ID);
+    });
+  });
+
+  describe('getBqDatasetLocation', () => {
+    it('should return the user property BQ_DATASET_LOCATION', () => {
+      const value = config.getBqDatasetLocation();
+      expect(value).toBe(TEST_USER_BQ_DATASET_LOCATION);
+    });
+  });
+
+  describe('getBqTableId', () => {
+    it('should return the user property BQ_TABLE_ID', () => {
+      const value = config.getBqTableId();
+      expect(value).toBe(TEST_USER_BQ_TABLE_ID);
+    });
+  });
+
+  describe('getGmailLabel', () => {
+    it('should return the user property GMAIL_LABEL', () => {
+      const value = config.getGmailLabel();
+      expect(value).toBe(TEST_USER_GMAIL_LABEL);
+    });
+  });
+
+  describe('getProcessingBatchSize', () => {
+    it('should return the user property PROCESSING_BATCH_SIZE', () => {
+      const value = config.getProcessingBatchSize();
+      expect(value).toBe(parseInt(TEST_USER_PROCESSING_BATCH_SIZE));
     });
   });
 });

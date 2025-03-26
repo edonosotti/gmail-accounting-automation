b1d0342cad03af99c28fdc4af1a6949bf0931aab
eddienst <edoardo.nosotti@gmail.com>
2025-03-26 01:25:52 +0100

    Update Logging class.

--- src/lib/logging.js
+++ src/lib/logging.js
@@ -1,11 +1,11 @@
-const logToCloud = (...args) => {
-  try {
+const logger = (...args) => {
+  if (typeof Logger !== "undefined") {
     // `Logger` is a global object provided by Google Apps Script.
     // It is used to log messages to the Stackdriver Logging service.
     // It is only available in the Google Apps Script environment.
     // eslint-disable-next-line no-undef
     Logger.log(...args);
-  } catch (_error) {
+  } else {
     // Fallback to `console.log` if `Logger` is not available.
     console.log(...args);
   }
@@ -13,10 +13,12 @@ const logToCloud = (...args) => {
 
 /**
  * This wrapper class allows to transparently switch between
- * logging to the console and logging to the Stackdriver Logging service
- * in Apps Script. It also allows local testing of the application
- * outside of Apps Script, trapping any errors that may occur because
- * of missing objects that only exist in the Apps Script environment.
+ * logging with `console` and the `Logger` object provided by
+ * Apps Script. Both will store the logs to Google Cloud Logging,
+ * but there are subtle differences between the two. Read the docs at:
+ * - https://developers.google.com/apps-script/guides/logging
+ * - https://developers.google.com/apps-script/reference/base/logger
+ * - https://developers.google.com/apps-script/reference/base/console
  */
 class Logging {
   /**
@@ -24,44 +26,44 @@ class Logging {
    * @param {Config} config Config reader object
    */
   constructor(config) {
-    this.useCloudLogging = config.getUserProp("USE_CLOUD_LOGGING", false);
+    this.useCloudLogger = config.getUserProp("USE_CLOUD_LOGGER", false);
   }
 
   log(...args) {
-    if (this.useCloudLogging) {
-      logToCloud(...args);
+    if (this.useCloudLogger) {
+      logger(...args);
     } else {
       console.log(...args);
     }
   }
 
   info(...args) {
-    if (this.useCloudLogging) {
-      logToCloud(...args);
+    if (this.useCloudLogger) {
+      logger(...args);
     } else {
       console.info(...args);
     }
   }
 
   warn(...args) {
-    if (this.useCloudLogging) {
-      logToCloud(...args);
+    if (this.useCloudLogger) {
+      logger(...args);
     } else {
       console.warn(...args);
     }
   }
 
   error(...args) {
-    if (this.useCloudLogging) {
-      logToCloud(...args);
+    if (this.useCloudLogger) {
+      logger(...args);
     } else {
       console.error(...args);
     }
   }
 
   debug(...args) {
-    if (this.useCloudLogging) {
-      logToCloud(...args);
+    if (this.useCloudLogger) {
+      logger(...args);
     } else {
       // `console.debug` is not available in the Google Apps Script environment.
       console.log(...args);

d7346be16bca2e03713af19a2c5797ddb147f66d
eddienst <edoardo.nosotti@gmail.com>
2025-03-26 02:01:58 +0100

    Add stub entry point for Google Apps Script.

---
+++ src/main.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+/**
+ * This file contains the main entry point of the application
+ * for Google Apps Script.
+ * It does not work in any other environment.
+ * The `eslint-disable no-undef` rule is required to ignore
+ * validation errors due to Google Apps Script automated
+ * loading of all objects into the global scope.
+ */
+
+/**
+ * Returns an instance of the dependency container
+ * with all dependencies registered.
+ * @return {DependencyContainer}
+ */
+const getDependencies = () => {
+  const diContainer = new DependencyContainer();
+  diContainer.register('logging', new Logging());
+}
+
+/**
+ * Main entry point of the application
+ * for Google Apps Script.
+ */
+const main = () => {
+  const diContainer = getDependencies();
+  const logging = diContainer.get('logging');
+  logging.log('Hey, world!');
+};
+
+module.exports = main;
